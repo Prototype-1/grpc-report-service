20cfa98b50d71987388f76051ace80742c072c0a
AswinPuthukaatil <aswin100396@gmail.com>
2025-06-17 22:09:54 +0530

    completed writing config module for loading variables from local .env

---
+++ config/config.go
@@ -0,0 +1,38 @@
+package config
+
+import (
+    "github.com/joho/godotenv"
+    "log"
+    "os"
+)
+
+type Config struct {
+    DBHost     string
+    DBPort     string
+    DBUser     string
+    DBPassword string
+    DBName     string
+    GRPCPort   string
+}
+
+func LoadConfig() Config {
+    if err := godotenv.Load(); err != nil {
+        log.Println("Error: .env file not found")
+    }
+
+    return Config{
+        DBHost:     getEnv("DB_HOST", "localhost"),
+        DBPort:     getEnv("DB_PORT", "5432"),
+        DBUser:     getEnv("DB_USER", "postgres"),
+        DBPassword: getEnv("DB_PASSWORD", "postgres"),
+        DBName:     getEnv("DB_NAME", "grpc_reports"),
+        GRPCPort:   getEnv("GRPC_PORT", "50051"),
+    }
+}
+
+func getEnv(key, fallback string) string {
+    if value := os.Getenv(key); value != "" {
+        return value
+    }
+    return fallback
+}
