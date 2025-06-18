4a12a016f9dbf1b4ece9806bf5c160d1c8720d85
AswinPuthukaatil <aswin100396@gmail.com>
2025-06-18 11:25:22 +0530

    wiredup everything to main.go and testing on postman

--- main.go
+++ main.go
@@ -1,1 +1,39 @@
 package main
+
+import (
+    "github.com/Prototype-1/grpc-report-service/config"
+    "github.com/Prototype-1/grpc-report-service/internal/repository"
+    "github.com/Prototype-1/grpc-report-service/internal/service"
+    "github.com/Prototype-1/grpc-report-service/pkg"
+   pb "github.com/Prototype-1/grpc-report-service/proto"
+    "go.uber.org/zap"
+    "google.golang.org/grpc"
+    "net"
+)
+
+func main() {
+    cfg := config.LoadConfig()
+
+    logger, _ := zap.NewProduction()
+    defer logger.Sync()
+
+    db := repository.InitPostgres(cfg, logger)
+
+    repo := repository.NewReportRepository(db, logger)
+    grpcService := service.NewReportService(repo, logger)
+
+    go pkg.StartScheduler(grpcService, logger)
+
+    lis, err := net.Listen("tcp", ":"+cfg.GRPCPort)
+    if err != nil {
+        logger.Fatal("Failed to listen", zap.Error(err))
+    }
+
+    s := grpc.NewServer()
+    pb.RegisterReportServiceServer(s, grpcService)
+
+    logger.Info("gRPC server started", zap.String("port", cfg.GRPCPort))
+    if err := s.Serve(lis); err != nil {
+        logger.Fatal("Failed to serve", zap.Error(err))
+    }
+}
